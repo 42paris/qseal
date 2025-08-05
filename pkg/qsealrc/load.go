b609380b159d60adf59638cdfa941987f852e540
Reach <reach@42paris.fr>
2025-08-05 23:18:02 +0200

    feat: search qsealrc.yaml in parent directories too

--- pkg/qsealrc/load.go
+++ pkg/qsealrc/load.go
@@ -3,17 +3,55 @@ package qsealrc
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/mcuadros/go-defaults"
 	"gopkg.in/yaml.v3"
 )
 
+func walkDir() (dir string, found bool, err error) {
+	dir, err = os.Getwd()
+	if err != nil {
+		return "", false, fmt.Errorf("error reading working directory")
+	}
+
+	for {
+		rcFullpath := filepath.Join(dir, QsealrcFileName)
+		_, err := os.Stat(rcFullpath)
+		if err == nil {
+			found = true
+			break
+		} else {
+			if !os.IsNotExist(err) {
+				return "", false, fmt.Errorf("error searching for %s: %w", QsealrcFileName, err)
+			}
+
+			nextDir := filepath.Dir(dir)
+			if nextDir == dir { // Stop at root dir
+				break
+			}
+			dir = nextDir
+		}
+	}
+
+	return dir, found, nil
+}
+
 func Load() (*Qsealrc, error) {
-	_, err := os.Stat(QsealrcFileName)
-	if os.IsNotExist(err) {
+	dir, found, err := walkDir()
+	if err != nil {
+		return nil, err
+	}
+	if !found {
 		return nil, fmt.Errorf("file %s does not exist", QsealrcFileName)
 	}
+
+	err = os.Chdir(dir)
+	if err != nil {
+		return nil, fmt.Errorf("could not change directory to %s: %w", dir, err)
+	}
+
 	file, err := os.Open(QsealrcFileName)
 	if err != nil {
 		return nil, err

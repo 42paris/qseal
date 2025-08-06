7f11ad2a206667b7ea5e8500b68f82fab6971847
Reach <reach@42paris.fr>
2025-08-06 03:11:30 +0200

    feat: add --ignore-parents/-i flag to qseal init

--- cmd/init.go
+++ cmd/init.go
@@ -9,7 +9,13 @@ var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initialize the qseal configuration file",
 	Run: func(cmd *cobra.Command, args []string) {
-		err := qsealrc.Init()
+		ignoreParents, err := cmd.PersistentFlags().GetBool("ignore-parents")
+		if err != nil {
+			cmd.PrintErrln("error reading command arguments:", err)
+			return
+		}
+
+		err = qsealrc.Init(ignoreParents)
 		if err != nil {
 			cmd.PrintErrln("error initializing configuration:", err)
 			return
